8b55fbb76ed9022855023727288392c139768d93
Salvatore Santamaria <sam.santamaria@gmail.com>
2020-12-26 14:43:03 -0800

    Handling text input and generating a password

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ɵCompiler_compileModuleSync__POST_R3__ } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,8 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Angular Password Generator';
-
+  includeLetters = false;
+  includeNumbers = false;
+  includeSymbols = false;
   passwordProperty = ""
+  length = 0;
 // the export class etc is equvalent to 
 // constructor() {
 //   this.password = ''
@@ -19,10 +22,53 @@ export class AppComponent {
   // function called anytime a user clicks on button
   onButtonClick() {
     //console.log('Button was clicked')
-    this.passwordProperty = "MY PASSWORD"
+    //this.passwordProperty = "MY PASSWORD"
+
+    const numbers = '1234567890'
+    const letters = 'abcdefghijklmnopqrstuvwxyz'
+    const symbols = '!@#$%^&*'
+    let validChars = '';
+    if (this.includeLetters) {
+      validChars += letters;
+    }
+    if (this.includeNumbers) {
+      validChars += numbers;
+    }
+    if (this.includeSymbols) {
+      validChars += symbols;
+    }
+
+    let generatedPassword = '';
+    for (let i = 0; i < this.length; i++) {
+      const index = Math.floor(Math.random() * validChars.length)
+      generatedPassword += validChars[index]
+    }
+    this.passwordProperty = generatedPassword;
+
   }
 
   // getPassword() {
   //   return this.passwordProperty
   // }
+
+  onLettersClick() {
+    this.includeLetters = !this.includeLetters
+  }
+
+  onNumbersClick() {
+    this.includeNumbers = !this.includeNumbers
+  }
+
+  onSymbolsClick() {
+    this.includeSymbols = !this.includeSymbols
+  }
+
+  onChangeLengthInput(value: string) {
+    this.length = parseInt(value);
+
+  }
+
+  interpolationFunctionCall() {
+    return 'value returned from app.component.ts interpolationFunctionCall()'
+  }
 }
